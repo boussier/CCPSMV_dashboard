b42054151ff99259faced350055a4ad4db4ff436
boussier <sylvain.boussier@gmail.com>
2021-01-11 08:41:17 +0100

    allow all hosts in django settings

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY",'jkahzjekhkazhejahzekjazhe')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG",True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
