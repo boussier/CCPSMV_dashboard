d473113730b8f84c5bab804ba62e6f8d2f117898
boussier <sylvain.boussier@gmail.com>
2021-04-22 10:09:31 +0200

    change docker ports configuration

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ RUN pip3 install -r requirements.txt
 
 COPY . /app/
 
-EXPOSE 8000
+EXPOSE 7072
--- scripts/start_dev_server.sh
+++ scripts/start_dev_server.sh
@@ -8,4 +8,4 @@ set -o xtrace
 python3 manage.py migrate
 python3 manage.py loaddata initial_data.json
 python3 manage.py collectstatic --noinput --verbosity 0
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:7072
